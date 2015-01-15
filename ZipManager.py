7641a2ce8ac15238e1f5d035a836a34c53ca08a7
testic2b@gmail.com <testic2b@gmail.com@2205c566-1b06-11df-b263-d5acc5e2fff2>
2015-01-15 10:58:57 +0000

    Separate the testable mixin from the container file

--- ZipManager.py
+++ ZipManager.py
@@ -259,7 +259,7 @@ class Zip:
 		zf = zipfile.ZipFile(fn, 'r')
 		nl = zf.namelist()
 		zf.close()
-		return any(map(lambda s: re.search("^(BDD/[\w*/]*\.py|BDD/[\w*/]*\.feature)$", s), nl))
+		return any(map(lambda s: re.search("^(BDD/[\w*/]*\.py|BDD/[\w*/]*\.spec)$", s), nl))
 
 	@staticmethod
 	def GetTests(fn):
@@ -271,7 +271,7 @@ class Zip:
 		zf.close()
 
 		###
-		tests_files = filter(lambda a: a!= [], map(lambda s:re.findall("^(BDD/[\w*/]*\.py|BDD/[\w*/]*\.feature)$", s), nl))
+		tests_files = filter(lambda a: a!= [], map(lambda s:re.findall("^(BDD/[\w*/]*\.py|BDD/[\w*/]*\.spec)$", s), nl))
 		tests_files = [a[0] for a in tests_files]
 
 		return tests_files
