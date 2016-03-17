29cf2c59f3d22c4b9a4bce2b85a3cde81f301a06
CelineBateauKessler <celinebateaukessler@gmail.com>
2016-03-17 16:39:15 +0100

    do not set simulation thread as daemon or it doesn't end correctly

--- version-2.9/SimulationGUI.py
+++ version-2.9/SimulationGUI.py
@@ -766,7 +766,6 @@ def simulator_factory(model, strategy, prof, ntl, verbose):
 			""" Constructor.
 			"""
 			threading.Thread.__init__(self)
-			self.daemon = True
 			Simulator.__init__(self, model)
 
 			### local copy
