8e25b372206fe96f0826c66038e5de42828eb954
capocchi_l <capocchi_l@UDC-3-0033.udcpp.priv>
2017-10-05 13:39:58 +0200

    -

--- version-2.9/devsimpy.py
+++ version-2.9/devsimpy.py
@@ -184,7 +184,7 @@ if not hasattr(sys, 'frozen'):
 			
 			### get get-pip.py file from DEVSimPy-site repository and install it
 			temp_directory = gettempdir()
-			downloadFile('https://raw.githubusercontent.com/capocchi/DEVSimPy-site/gh-pages/pip-packages/get-pip.py', temp_directory)
+			downloadFile('https://raw.githubusercontent.com/capocchi/DEVSimPy-site/gh-pages/get-pip.py', temp_directory)
 			os.system('{} {}'.format('python', os.path.join(temp_directory, 'get-pip.py')))
 			
 		### find the CPU architecture
