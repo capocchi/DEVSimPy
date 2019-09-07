8f21b9d87c7267727095be15ba38f8c7f0bd1641
Capocchi L <capocchi@univ-corse.fr>
2019-09-07 14:30:37 +0200

    bug fix in PyDEVS/simulation.py for super

--- DEVSKernel/PyDEVS/simulator.py
+++ DEVSKernel/PyDEVS/simulator.py
@@ -205,7 +205,7 @@ class CoupledSolver(Sender):
 	_instance = None
 	def __new__(cls, *args, **kwargs):
 		if not cls._instance:
-			cls._instance = super(AtomicSolver, cls).__new__(cls,*args, **kwargs)
+			cls._instance = super(Sender, cls).__new__(cls,*args, **kwargs)
 		return cls._instance
 
 	def threading_send(self, Y, cDEVS, t):
