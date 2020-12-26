b9ab9616896ddc7611c8d7cb591de9b6f32c38bf
Capocchi L <capocchi@univ-corse.fr>
2020-12-26 23:26:38 +0100

    update

--- SimulationNoGUI.py
+++ SimulationNoGUI.py
@@ -15,6 +15,8 @@ GLOBAL VARIABLES AND FUNCTIONS:
 import os
 import sys
 import time
+import threading
+
 
 import __builtin__
 from cStringIO import StringIO
@@ -27,6 +29,9 @@ _ = gettext.gettext
 import InteractionSocket
 import json
 import pusher
+
+from Decorators import hotshotit
+from Patterns.Strategy import *
 
 sys.path.append(os.path.join('Domain', 'Phidgets'))
 
@@ -227,9 +232,150 @@ class runSimulation:
         ################################################################################################################
         #print __builtin__.__dict__
         if self.master:
-            from SimulationGUI import simulator_factory
+            #from SimulationGUI import simulator_factory
             if not self.ntl:
                 self.master.FINAL_TIME = float(self.time)
             self.thread = simulator_factory(self.master, self.selected_strategy, self.prof, self.ntl, self.verbose, self.dynamic_structure_flag, self.real_time_flag)
 
             return self.thread
+
+def simulator_factory(model, strategy, prof, ntl, verbose, dynamic_structure_flag, real_time_flag):
+	""" Preventing direct creation for Simulator
+        disallow direct access to the classes
+	"""
+
+	### find the correct simulator module depending on the
+	for pydevs_dir, filename in __builtin__.__dict__['DEVS_DIR_PATH_DICT'].items():
+		if pydevs_dir == __builtin__.__dict__['DEFAULT_DEVS_DIRNAME']:
+			from DEVSKernel.PyDEVS.simulator import Simulator as BaseSimulator
+
+	class Simulator(BaseSimulator):
+		"""
+		"""
+		###
+		def __init__(self, model):
+			"""Constructor.
+			"""
+
+			BaseSimulator.__init__(self, model)
+
+			self.model = model
+			self.__algorithm = SimStrategy1(self)
+
+		def simulate(self, T = sys.maxint):
+			return self.__algorithm.simulate(T)
+
+		def getMaster(self):
+			return self.model
+
+		def setMaster(self, model):
+			self.model = model
+
+		def setAlgorithm(self, s):
+			self.__algorithm = s
+
+		def getAlgorithm(self):
+			return self.__algorithm
+
+	class SimulationThread(threading.Thread, Simulator):
+		"""
+			Thread for DEVS simulation task
+		"""
+
+		def __init__(self, model = None, strategy = '', prof = False, ntl = False, verbose=False, dynamic_structure_flag=False, real_time_flag=False):
+			""" Constructor.
+			"""
+			threading.Thread.__init__(self)
+			Simulator.__init__(self, model)
+
+			### local copy
+			self.strategy = strategy
+			self.prof = prof
+			self.ntl = ntl
+			self.verbose = verbose
+			self.dynamic_structure_flag = dynamic_structure_flag
+			self.real_time_flag = real_time_flag
+
+			self.end_flag = False
+			self.thread_suspend = False
+			self.sleep_time = 0.0
+			self.thread_sleep = False
+			self.cpu_time = -1
+
+			self.start()
+
+		@hotshotit
+		def run(self):
+			""" Run thread
+			"""
+
+			### define the simulation strategy
+			args = {'simulator':self}
+			### TODO: isinstance(self, PyDEVSSimulator)
+			if DEFAULT_DEVS_DIRNAME == "PyDEVS":
+				cls_str = eval(PYDEVS_SIM_STRATEGY_DICT[self.strategy])
+			else:
+				cls_str = eval(PYPDEVS_SIM_STRATEGY_DICT[self.strategy])
+
+			self.setAlgorithm(apply(cls_str, (), args))
+
+			while not self.end_flag:
+				### traceback exception engine for .py file
+				try:
+					self.simulate(self.model.FINAL_TIME)
+				except Exception, info:
+					self.terminate(error=True, msg=sys.exc_info())
+
+		def terminate(self, error = False, msg = None):
+			""" Thread termination routine
+				param error: False if thread is terminate without error
+				param msg: message to submit
+			"""
+
+			if not self.end_flag:
+				if error:
+
+					###for traceback
+					etype = msg[0]
+					evalue = msg[1]
+					etb = traceback.extract_tb(msg[2])
+					sys.stderr.write('Error in routine: your routine here\n')
+					sys.stderr.write('Error Type: ' + str(etype) + '\n')
+					sys.stderr.write('Error Value: ' + str(evalue) + '\n')
+					sys.stderr.write('Traceback: ' + str(etb) + '\n')
+
+					### only for displayed application (-nogui)
+					#if wx.GetApp():
+					#	wx.CallAfter(Publisher.sendMessage, "error", msg)
+						### error sound
+					#	wx.CallAfter(playSound, SIMULATION_ERROR_SOUND_PATH)
+
+				else:
+					for m in filter(lambda a: hasattr(a, 'finish'), self.model.componentSet):
+						### call finished method
+						if __builtin__.__dict__['GUI_FLAG']:
+							Publisher.sendMessage('%d.finished'%(id(m)))
+						else:
+							m.finish(None)
+
+					### only for displayed application (-nogui)
+					#if wx.GetApp() : wx.CallAfter(playSound, SIMULATION_SUCCESS_SOUND_PATH)
+
+			self.end_flag = True
+
+		def set_sleep(self, sleeptime):
+			self.thread_sleep = True
+			self._sleeptime = sleeptime
+
+		def suspend(self):
+
+			#main_thread = threading.currentThread()
+			#for t in threading.enumerate():
+			#	t.thread_suspend = True
+
+			self.thread_suspend = True
+
+		def resume_thread(self):
+			self.thread_suspend = False
+
+	return SimulationThread(model, strategy, prof, ntl, verbose, dynamic_structure_flag, real_time_flag)
