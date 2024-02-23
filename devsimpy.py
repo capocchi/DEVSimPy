d2c276d31d963deb27be3a23c398626721e344ea
Capocchi <lcapocchi@gmail.com>
2024-02-23 14:13:05 +0100

    update

--- devsimpy.py
+++ devsimpy.py
@@ -1551,7 +1551,7 @@ class MainApplication(wx.Frame):
 
 		### try to save the current diagram into a temp yaml file and launch the diag for the standalone exportation 
 		if Container.Diagram.SaveFile(diagram, temp_yaml_path):
-			frame = StandaloneGUI(None, -1, _('Standalone settings'), yaml=temp_yaml_path)
+			frame = StandaloneGUI(None, -1, _('Standalone Settings'), yaml=temp_yaml_path)
 			frame.Show(True)
 		else:
 			wx.MessageBox(_("An error occurred during the saving as yaml file."), _("Error"), wx.OK | wx.ICON_ERROR)
