fbde16d9e7f1b774bef7dac4880771a137873d4c
Capocchi <lcapocchi@gmail.com>
2025-04-08 15:04:59 +0200

    bug fix

--- tests/test_xmlmodule.py
+++ tests/test_xmlmodule.py
@@ -5,7 +5,8 @@ Usage:
     python test_xmlmodule.py --autoclose 10  # Auto-close after 10s delay
 """
 
-import wx, os
+import wx
+import os
 
 from ApplicationController import TestApp
 
@@ -18,14 +19,14 @@ import DetachedFrame
 app = TestApp(0)
 
 diagram = Container.Diagram()
-        
+
 frame = DetachedFrame.DetachedFrame(None, -1, "Test", diagram)
 newPage = Container.ShapeCanvas(frame, wx.NewIdRef(), name='Test')
 newPage.SetDiagram(diagram)
 
-path = os.path.join(os.path.expanduser("~"),'Downloads','Watershed.xml')
-#path = os.path.join(os.path.expanduser("~"),'Downloads','example.xmlsestree')
+path = os.path.join(os.path.expanduser("~"), 'Downloads', 'Watershed.xml')
+# path = os.path.join(os.path.expanduser("~"),'Downloads','example.xmlsestree')
 getDiagramFromXMLSES(path, canvas=newPage)
-#diagram.SetParent(newPage)
+# diagram.SetParent(newPage)
 
 app.RunTest(frame)
