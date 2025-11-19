50b57226120b02c23cde022b4e9f27b1871825ce
Capocchi <lcapocchi@gmail.com>
2025-11-20 00:34:46 +0100

    write log of kafka trace in logs dir

--- devsimpy/DEVSKernel/KAFKADEVS/logconfig.py
+++ devsimpy/DEVSKernel/KAFKADEVS/logconfig.py
@@ -1,5 +1,7 @@
 ### Fichier de configuration du logging pour KafkaDEVS
 import logging
+import os, sys
+from pathlib import Path
 
 ### Niveau de logging:
 ###  - logging.DEBUG pour le dev
@@ -9,13 +11,20 @@ import logging
 LOGGING_LEVEL = logging.DEBUG
 
 def configure_logging():
+    # Racine DEVSimPy = dossier contenant devsimpy-nogui.py / devsimpy.py
+    base_dir = Path(__file__).resolve().parents[2]   # DEVSimPy/
+    log_dir = base_dir / "logs"
+    log_dir.mkdir(exist_ok=True)
+
+    log_file = log_dir / "kafkadevs.log"
+
     logging.basicConfig(
-        level=logging.DEBUG,  # DEBUG pour le dev et WARNING pour la prod
+        level=LOGGING_LEVEL,
         format="%(asctime)s [%(levelname)s] %(name)s - %(message)s",
         handlers=[
-            logging.StreamHandler(),                    # console
-            logging.FileHandler("kafka_devssim.log")    # fichier
-        ]
+            logging.FileHandler(log_file, encoding="utf-8"),
+            logging.StreamHandler(),   # console (optionnel)
+        ],
     )
 
 # logger dédié pour les traces Kafka
